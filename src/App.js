ba489993db18d248b947eb482d1d381a518a87db
Grecia Delgado <greciadm2596@gmail.com>
2021-03-01 23:07:10 -0500

    now we can show to the user dogs pictures

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [dogs, setDogsList] = useState([]);
+  const [breeds, setBreedList] = useState([]);
+
+  useEffect(() => {
+    const fetchDogs = async () => {
+      const response = await fetch(
+        "https://dog.ceo/api/breeds/image/random/10"
+      );
+      const data = await response.json();
+      setDogsList(data.message);
+    };
+    fetchDogs();
+  }, []);
+
+  useEffect(() => {
+    const fetchBreeds = async () => {
+      const response = await fetch("https://dog.ceo/api/breeds/list/all");
+      const data = await response.json();
+      setBreedList(Object.keys(data.message));
+    };
+    fetchBreeds();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <i className="fas fa-paw"></i>
+        <h1>THE DOGS</h1>
+        <i className="fas fa-paw"></i>
       </header>
+      <label htmlFor="breed" style={{marginRight:'5px'}}>Breed:</label>
+      <select id="breed" className="select">
+        <option selected disabled>
+          Choose breed
+        </option>
+        {breeds.map((breed, index) => (
+          <option className="option" key={index}>
+            {breed}
+          </option>
+        ))}
+      </select>
+      <div className="gallery">
+        {dogs.map((dog, index) => (
+          <img key={index} src={dog} style={{ width: "100%" }} />
+        ))}
+      </div>
     </div>
   );
 }
---
+++ src/apiFetch.js
@@ -0,0 +1,5 @@
+async function apiFetch(...args) {
+  const response = await fetch(...args)
+  const data = await response.json()
+  return data;
+}
