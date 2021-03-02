161c45b01642afc3f6c8798e85fde27811439b90
Grecia Delgado <greciadm2596@gmail.com>
2021-03-02 00:02:36 -0500

    now we can show a list of especifis breed of dog

--- src/App.js
+++ src/App.js
@@ -4,17 +4,26 @@ import "./App.css";
 function App() {
   const [dogs, setDogsList] = useState([]);
   const [breeds, setBreedList] = useState([]);
+  const [currentBreed, setCurrentBreed] = useState('all')
 
   useEffect(() => {
     const fetchDogs = async () => {
-      const response = await fetch(
-        "https://dog.ceo/api/breeds/image/random/10"
-      );
-      const data = await response.json();
-      setDogsList(data.message);
+      if(currentBreed === 'all') {
+        const response = await fetch(
+          "https://dog.ceo/api/breeds/image/random/10"
+        );
+        const data = await response.json();
+        setDogsList(data.message);
+      } else {
+        const response = await fetch(
+          `https://dog.ceo/api/breed/${currentBreed}/images/random/10`
+        );
+        const data = await response.json();
+        setDogsList(data.message);
+      }
     };
     fetchDogs();
-  }, []);
+  }, [currentBreed]);
 
   useEffect(() => {
     const fetchBreeds = async () => {
@@ -25,6 +34,16 @@ function App() {
     fetchBreeds();
   }, []);
 
+  // const handleChange = async (e) => {
+  //   console.log(e.target.value)
+  
+  //   const response = await fetch(
+  //     `https://dog.ceo/api/breed/${e.target.value}/images/random/10`
+  //   );
+  //   const data = await response.json();
+  //   setDogsList(data.message);
+  // };
+
   return (
     <div className="App">
       <header className="App-header">
@@ -32,13 +51,15 @@ function App() {
         <h1>THE DOGS</h1>
         <i className="fas fa-paw"></i>
       </header>
-      <label htmlFor="breed" style={{marginRight:'5px'}}>Breed:</label>
-      <select id="breed" className="select">
-        <option selected disabled>
-          Choose breed
+      <label htmlFor="breed" style={{ marginRight: "5px" }}>
+        Breed:
+      </label>
+      <select id="breed" className="select" onChange={(e) => setCurrentBreed(e.target.value)}>
+        <option selected value="all">
+          Choose a Breed
         </option>
         {breeds.map((breed, index) => (
-          <option className="option" key={index}>
+          <option className="option" value={breed} key={index}>
             {breed}
           </option>
         ))}
